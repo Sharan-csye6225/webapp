4b91c164d20199a16e74cbc1960c3c6e113848bd
SharanKumarPrabhakaran <prabhakaran.sh@northeastern.edu>
2024-02-13 18:50:58 -0500

    Added Test comment

--- src/main/java/com/cloudcomputing/csye6225/Utils/CommonUtil.java
+++ src/main/java/com/cloudcomputing/csye6225/Utils/CommonUtil.java
@@ -3,7 +3,7 @@ package com.cloudcomputing.csye6225.utils;
 import org.springframework.http.HttpHeaders;
 
 public class CommonUtil {
-
+//test comment
     // Static utility method to set all the header variables for the API response
     public static HttpHeaders setHeaders() {
         HttpHeaders headers = new HttpHeaders();
