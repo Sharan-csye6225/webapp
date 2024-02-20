19c3d38c510c82b4b409d5e63d08b835bbace7e1
SharanKumarPrabhakaran <prabhakaran.sh@northeastern.edu>
2024-02-20 10:50:10 -0500

    Removed maven setup and update bash file

--- packer/install_dependencies.sh
+++ packer/install_dependencies.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Intallation Depedency Script file execution started."
 
-# Install MySQL (assuming MySQL is what you want, not MariaDB)
+sudo yum update -y
+
 sudo yum install -y mysql-server
 
 echo "MySQL install completed"
 
-# Start MySQL service
 sudo systemctl start mysqld
 
-# Enable MySQL to start on boot
 sudo systemctl enable mysqld
 
 sudo mysql -u root <<EOF
@@ -18,29 +17,17 @@ FLUSH PRIVILEGES;
 exit
 EOF
 
-# Install Java JDK 17
 sudo yum install -y java-17-openjdk-devel
 
-#To see the Java version installed
 java -version
 
 echo "JDK install completed"
 
-# Install Maven
-sudo yum install -y maven
+echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk' >>~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >>~/.bashrc
 
-#To see the Maven version installed
-mvn -version
-
-echo "Maven install completed"
-
-echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk' >>~/.bash_profile
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >>~/.bash_profile
-
-echo 'export M2_HOME=/opt/mavenk' >>~/.bash_profile
-echo 'export PATH=$M2_HOME/bin:$PATH' >>~/.bash_profile
-
-source ~/.bash_profile
+cat ~/.bashrc
+sudo source ~/.bashrc
 
 cd /opt/
 sudo mkdir cloud/
